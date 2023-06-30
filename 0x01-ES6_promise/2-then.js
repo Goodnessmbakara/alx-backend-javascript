8aa8b4a8695c6a2258f30bc3b58acde22e32607d
Goodnessmbakara <amicablembakara50@gmail.com>
2023-06-30 01:28:38 +0100

    who knows

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  const data = { status: 200, body: 'success' };
-  promise
-    .then(() => data)
-    .catch(() => { new Error(''); })
-    .finally(() => {
-      console.log('Got a response from the API ');
-    });
+  promise.then((value) => ({
+    status: 200,
+    body: 'success',
+  }));
+
+  promise.catch((error) => ({}));
+
+  promise.then((value) => {
+    console.log('Got a response from the API');
+  });
 }
