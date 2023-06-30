8ea50f5a8a1bab983e6e556cf535b52d2f042e23
Goodnessmbakara <amicablembakara50@gmail.com>
2023-06-30 02:56:16 +0100

    took code

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promise1 = uploadPhoto();
-  const promise2 = createUser();
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [uploadPhotoResult, createUserResult] = results;
-      console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
-    });
-  promise1.catch((error) => {
-    // The photo upload failed.
-    console.log('Signup system offline', error.message);
-  });
-  promise2.catch((error) => {
-    // The user creation failed.
-    console.log('Signup system offline', error.message);
-  });
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
