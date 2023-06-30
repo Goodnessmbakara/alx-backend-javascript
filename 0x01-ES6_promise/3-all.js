17c01f479b78cfc026c9aa32ded7511236179c08
Goodnessmbakara <amicablembakara50@gmail.com>
2023-06-30 02:52:36 +0100

    catcher

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
   Promise.all([uploadPhoto(), createUser()])
-    .then(results => {
+    .then((results) => {
       const [uploadPhotoResult, createUserResult] = results;
       console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
-    })
-    .catch(error => {
-      console.error('Signup system offline');
     });
+  promise1.catch((error) => {
+    // The photo upload failed.
+    console.log('Signup system offline', error.message);
+  });
+  promise2.catch((error) => {
+    // The user creation failed.
+    console.log('Signup system offline', error.message);
+  });
 }
