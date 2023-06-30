7483f2d405ba3fd6d0bda0ba7b3e515cd1b2ea23
Goodnessmbakara <amicablembakara50@gmail.com>
2023-06-30 17:31:12 +0100

    I need to get done with this promises thingy

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,14 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default handleProfileSignup(firstName,lastName,fileName){
-	return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(results => {
-[photoResult,userResult] = results;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const [userResult, photoResult] = results;
 
-console.log(`User Result - Status: ${userResult.status}, Value: ${userResult.value}`);
-
-      // Accessing status and value of photoResult
+      console.log(`User Result - Status: ${userResult.status}, Value: ${userResult.value}`);
       console.log(`Photo Result - Status: ${photoResult.status}, Value: ${photoResult.value}`);
 
-	    return [photoResult,userResult];
-
-    {
-     ` status: ${Promise.status}`,
-     ` value: ${results}`
-    },
-  ]
-    };
+      return { userResult, photoResult };
+    });
 }
