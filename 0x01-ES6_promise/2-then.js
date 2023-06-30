3b0e92a8f4b44b37d4f7163639da3b84cc92bae5
Goodnessmbakara <amicablembakara50@gmail.com>
2023-06-30 01:22:54 +0100

    i think this works

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   const data = { status: 200, body: 'success' };
   promise
-    .then(() => (data))
+    .then(() => data)
     .catch(() => { new Error(''); })
     .finally(() => {
       console.log('Got a response from the API ');
